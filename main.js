a9202bef4ff411faf84d89a5ea3348df2ac3f9f0
Rishabh-Jain123 <88461447+Rishabh-Jain123@users.noreply.github.com>
2021-08-04 17:25:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var first = "My First Console Project";
+
+console.log(first);
+
+var first_name = "Jake";
+
+var last_name = "Jones";
+
+var full_name = first_name + last_name ;
+
+console.log(full_name);
+
+var a = 10;
+
+var b = 5;
+
+var add = a + b;
+
+console.log(add);
+
+var minus = a - b;
+
+console.log(minus);
+
+var multiply = a * b;
+
+console.log(multiply);
+
+var divide = a / b;
+
+console.log(divide);
